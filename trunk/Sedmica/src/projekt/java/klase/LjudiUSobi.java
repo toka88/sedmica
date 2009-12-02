f857fa547b923e439f813b3a3348abd13deaeb68
code.blueproject <code.blueproject@255ce1ec-a2a5-11de-be8d-735280eae207>
2009-12-02 15:10:08 +0000

    Nova klasa na strani klijenta

---
+++ trunk/Sedmica/src/projekt/java/klase/LjudiUSobi.java
@@ -0,0 +1,18 @@
+package projekt.java.klase;
+
+public class LjudiUSobi {
+	String[] ljudiUSobi;
+	int maxLjudi = 4;
+	public String[] getLjudiUSobi() {
+		return ljudiUSobi;
+	}
+	public void setLjudiUSobi(String[] ljudiUSobi) {
+		this.ljudiUSobi = ljudiUSobi;
+	}
+	public int getMaxLjudi() {
+		return maxLjudi;
+	}
+	public void setMaxLjudi(int maxLjudi) {
+		this.maxLjudi = maxLjudi;
+	}
+}
